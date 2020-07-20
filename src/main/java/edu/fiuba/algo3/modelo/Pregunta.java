9edd82ebafb1f1954e64e146bbedcb4d4263a707
TomiGalian <tomiezeg@gmail.com>
2020-07-20 18:42:31 -0300

    Agrego interfaz Pregunta

---
+++ src/main/java/edu/fiuba/algo3/modelo/Pregunta.java
@@ -0,0 +1,6 @@
+package edu.fiuba.algo3.modelo;
+
+public interface Pregunta {
+    boolean evaluar(Respuesta unaRespuesta);
+    boolean respuesta();
+}
--- src/main/test/edu/fiuba/algo3/modelo/VerdaderoFalsoTest.java
+++ src/main/test/edu/fiuba/algo3/modelo/VerdaderoFalsoTest.java
@@ -8,7 +8,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class Verdadero_FalsoTest {
+public class VerdaderoFalsoTest {
 
     @Test
     public void test00LasPreguntasSeCreanCorrectamente() {
