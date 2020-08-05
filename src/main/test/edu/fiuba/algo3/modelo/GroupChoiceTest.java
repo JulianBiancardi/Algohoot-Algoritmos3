2bea90f672628acf3523be7c662e375d15f16818
leotarditi <ltarditi@fi.uba.ar>
2020-08-05 16:08:52 -0300

    Actualizo pruebas GroupChoice

--- src/main/test/edu/fiuba/algo3/modelo/GroupChoiceTest.java
+++ src/main/test/edu/fiuba/algo3/modelo/GroupChoiceTest.java
@@ -11,7 +11,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class GroupChoiceTest {
 
     @Test
-    public void test01AmbosGruposCon2RespuestasCorrectasCadaUnoRespondeCorrectamente(){
+    public void test01AmbosGruposConDosRespuestasCorrectasCadaUnoRespondeCorrectamente(){
 
         String nombreGrupo1 = "Numeros Pares";
         String nombreGrupo2 = "Numeros Impares";
@@ -35,7 +35,7 @@ public class GroupChoiceTest {
     }
 
     @Test
-    public void test02AmbosGruposCon2RespuestasCorrectasCadaUnoRespondeIncorrectamente(){
+    public void test02AmbosGruposConDosRespuestasCorrectasCadaUnoRespondeIncorrectamente(){
 
         String nombreGrupo1 = "Numeros Pares";
         String nombreGrupo2 = "Numeros Impares";
@@ -80,5 +80,107 @@ public class GroupChoiceTest {
 
         pregunta.evaluarRespuestas(respuestas);
         assertEquals(1, jugador.puntos());
+    }
+
+    @Test
+    public void test04AmbosGruposConDosRespuestasCorrectasCadaUnoRespondenAmbosCorrectamente(){
+
+        String nombreGrupo1 = "Numeros Pares";
+        String nombreGrupo2 = "Numeros Impares";
+        GroupChoice pregunta = new GroupChoice("Separar en Pares e Impares",nombreGrupo1, nombreGrupo2);
+        pregunta.agregarOpcionGrupo1(new OpcionGrupal("2"));
+        pregunta.agregarOpcionGrupo2(new OpcionGrupal("3"));
+        pregunta.agregarOpcionGrupo1(new OpcionGrupal("4"));
+        pregunta.agregarOpcionGrupo2(new OpcionGrupal("5"));
+        Jugador jugador1 = new Jugador("LeoProgramador");
+        Jugador jugador2 = new Jugador("Julianaso");
+        RespuestaGrupal respuestaJugador1 = new RespuestaGrupal(jugador1, nombreGrupo1, nombreGrupo2);
+        RespuestaGrupal respuestaJugador2 = new RespuestaGrupal(jugador2, nombreGrupo1, nombreGrupo2);
+
+        //Responde jugador1
+        respuestaJugador1.agregarOpcionGrupo1(pregunta.obtenerOpcion(0));
+        respuestaJugador1.agregarOpcionGrupo1(pregunta.obtenerOpcion(2));
+        respuestaJugador1.agregarOpcionGrupo2(pregunta.obtenerOpcion(1));
+        respuestaJugador1.agregarOpcionGrupo2(pregunta.obtenerOpcion(3));
+        //Responde jugador2
+        respuestaJugador2.agregarOpcionGrupo1(pregunta.obtenerOpcion(0));
+        respuestaJugador2.agregarOpcionGrupo1(pregunta.obtenerOpcion(2));
+        respuestaJugador2.agregarOpcionGrupo2(pregunta.obtenerOpcion(1));
+        respuestaJugador2.agregarOpcionGrupo2(pregunta.obtenerOpcion(3));
+        ArrayList<Respuesta> respuestas = new ArrayList<Respuesta>();
+        respuestas.add(respuestaJugador1);
+        respuestas.add(respuestaJugador2);
+
+        pregunta.evaluarRespuestas(respuestas);
+        assertEquals(1, jugador1.puntos());
+        assertEquals(1, jugador2.puntos());
+    }
+
+    @Test
+    public void test05AmbosGruposConDosRespuestasCorrectasCadaUnoAmbosRespondeIncorrectamente(){
+
+        String nombreGrupo1 = "Numeros Pares";
+        String nombreGrupo2 = "Numeros Impares";
+        GroupChoice pregunta = new GroupChoice("Separar en Pares e Impares",nombreGrupo1, nombreGrupo2);
+        pregunta.agregarOpcionGrupo1(new OpcionGrupal("2"));
+        pregunta.agregarOpcionGrupo2(new OpcionGrupal("3"));
+        pregunta.agregarOpcionGrupo1(new OpcionGrupal("4"));
+        pregunta.agregarOpcionGrupo2(new OpcionGrupal("5"));
+        Jugador jugador1 = new Jugador("LeoProgramador");
+        Jugador jugador2 = new Jugador("Joaco");
+        RespuestaGrupal respuestaJugador1 = new RespuestaGrupal(jugador1, nombreGrupo1, nombreGrupo2);
+        RespuestaGrupal respuestaJugador2 = new RespuestaGrupal(jugador2, nombreGrupo1, nombreGrupo2);
+
+        //Responde jugador1
+        respuestaJugador1.agregarOpcionGrupo1(pregunta.obtenerOpcion(0));
+        respuestaJugador1.agregarOpcionGrupo1(pregunta.obtenerOpcion(1));
+        respuestaJugador1.agregarOpcionGrupo2(pregunta.obtenerOpcion(2));
+        respuestaJugador1.agregarOpcionGrupo2(pregunta.obtenerOpcion(3));
+        //Responde jugador2
+        respuestaJugador2.agregarOpcionGrupo1(pregunta.obtenerOpcion(3));
+        respuestaJugador2.agregarOpcionGrupo1(pregunta.obtenerOpcion(1));
+        respuestaJugador2.agregarOpcionGrupo2(pregunta.obtenerOpcion(0));
+        respuestaJugador2.agregarOpcionGrupo2(pregunta.obtenerOpcion(2));
+        ArrayList<Respuesta> respuestas = new ArrayList<Respuesta>();
+        respuestas.add(respuestaJugador1);
+        respuestas.add(respuestaJugador2);
+
+        pregunta.evaluarRespuestas(respuestas);
+        assertEquals(0, jugador1.puntos());
+        assertEquals(0, jugador2.puntos());
+    }
+
+    @Test
+    public void test06GruposConDistintaCantidadDeOpcionesUnoRespondeCorrectamenteYOtroNo(){
+
+        String nombreGrupo1 = "Numeros Pares";
+        String nombreGrupo2 = "Numeros Impares";
+        GroupChoice pregunta = new GroupChoice("Separar en Pares e Impares",nombreGrupo1, nombreGrupo2);
+        pregunta.agregarOpcionGrupo1(new OpcionGrupal("2"));
+        pregunta.agregarOpcionGrupo1(new OpcionGrupal("4"));
+        pregunta.agregarOpcionGrupo1(new OpcionGrupal("6"));
+        pregunta.agregarOpcionGrupo2(new OpcionGrupal("5"));
+        Jugador jugador1 = new Jugador("LeoProgramador");
+        Jugador jugador2 = new Jugador("Fede");
+        RespuestaGrupal respuestaJugador1 = new RespuestaGrupal(jugador1, nombreGrupo1, nombreGrupo2);
+        RespuestaGrupal respuestaJugador2 = new RespuestaGrupal(jugador2, nombreGrupo1, nombreGrupo2);
+
+        //Responde jugador1
+        respuestaJugador1.agregarOpcionGrupo1(pregunta.obtenerOpcion(3));
+        respuestaJugador1.agregarOpcionGrupo1(pregunta.obtenerOpcion(1));
+        respuestaJugador1.agregarOpcionGrupo1(pregunta.obtenerOpcion(0));
+        respuestaJugador1.agregarOpcionGrupo2(pregunta.obtenerOpcion(2));
+        //Responde jugador2
+        respuestaJugador2.agregarOpcionGrupo1(pregunta.obtenerOpcion(0));
+        respuestaJugador2.agregarOpcionGrupo1(pregunta.obtenerOpcion(1));
+        respuestaJugador2.agregarOpcionGrupo1(pregunta.obtenerOpcion(2));
+        respuestaJugador2.agregarOpcionGrupo2(pregunta.obtenerOpcion(3));
+        ArrayList<Respuesta> respuestas = new ArrayList<Respuesta>();
+        respuestas.add(respuestaJugador1);
+        respuestas.add(respuestaJugador2);
+
+        pregunta.evaluarRespuestas(respuestas);
+        assertEquals(0, jugador1.puntos());
+        assertEquals(1, jugador2.puntos());
     }
 }
